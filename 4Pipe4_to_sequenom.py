b7ea27591593afac0f7b932a57a5a52e5725d29f
Francisco Pina Martins <f.pinamartins@gmail.com>
2013-05-09 11:34:49 +0100

    Implemented the bam_miner funciotion. There is still a bug with the treshold.

--- 4Pipe4_to_sequenom.py
+++ 4Pipe4_to_sequenom.py
@@ -16,9 +16,11 @@
 #
 #Usage: python2 4Pipe4_to_sequenom.py infile.bam infile.fasta variation_treshold(float: 0-1)
 
+from __future__ import division
 import pysam
 import re
 from sys import argv
+
 
 def FASTAtoDict(fasta_file):
     #This will convert the fasta file into a dict like: "name":"seq" and return it
@@ -37,41 +39,31 @@ def FASTAtoDict(fasta_file):
 def bam_miner(samfile, usable_snps, var_treshold):
     infile = pysam.Samfile(samfile, "rb" )
     var_treshold = float(var_treshold)
+    selected_contigs = usable_snps
 
     for contig,snps in usable_snps.items():
         for snp in snps:
-            snp = int(re.match("(\d+)", snp).group())
-            seq_range = set(range(snp - 100, snp) + range(snp + 1, snp + 101))
+            snp_pos = int(re.match("(\d+)", snp).group())
+            seq_range = set(range(snp_pos - 100, snp_pos) + range(snp_pos + 1, snp_pos + 101))
             for pileupcolumn in infile.pileup(contig):
                 if pileupcolumn.pos in seq_range:
-                    print "Change"
+                    bases = {"A":0,"C":0,"G":0,"T":0}
                     for pileupread in pileupcolumn.pileups:
-                        print pileupread.alignment.seq[pileupread.qpos]
-                        #TODO: Change print to a counter
+                        if pileupread.alignment.seq[pileupread.qpos] in bases:
+                            bases[pileupread.alignment.seq[pileupread.qpos]] += 1
+                    raw_numbers = list(bases.values())
+                    most_frequent = max(raw_numbers)
+                    var_rate = most_frequent / sum(raw_numbers)
+                    if var_rate <= 1 - (var_treshold / 100):
+                        if snp in selected_contigs[contig]:
+                            selected_contigs[contig].remove(snp)
 
-        #for ref in infile.references:
-            #SNPs = {}
-            #raw_snps = ref.split("#")[1:]
-            #for i in raw_snps:
-                #proc_snps = re.split("(\d+)", i)
-                #SNPs[int(proc_snps[1])-1]=set(list(proc_snps[2])) #-1 due to index 0!
-                #how_many = len(SNPs)
-            #for pileupcolumn in infile.pileup(ref):
-                #if pileupcolumn.pos in SNPs:
-                    #how_many -= 1
-                    #depth = pileupcolumn.n
-                    #if depth < min_depth:
-                        #Invalidable_snps += 1
-                        #continue
-                    #bases = set()
-                    #for pileupread in pileupcolumn.pileups:
-                        #bases.add(pileupread.alignment.seq[pileupread.qpos])
-                    #if len(SNPs[pileupcolumn.pos].intersection(bases)) >= 2:
-                        #Valid_snps += 1
-                    #else:
-                        #Invalid_snps += 1
-            #if how_many != 0:
-                #Invalidable_snps += how_many
+    #Remove contigs with no SNPs:
+    for x in list(selected_contigs.keys()):
+        if selected_contigs[x] == []:
+            del selected_contigs[x]
+    print len(selected_contigs)
+    #TODO: Generate the Fastas and we're done!
 
 
 
